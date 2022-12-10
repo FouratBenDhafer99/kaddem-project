4522e649ee85a5f20b09273a2fffce4a56efde15
Fourat Ben Dhafer <fouratbendhafer99@gmail.com>
2022-12-10 18:27:56 +0100

    Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UniversiteListComponent } from './universite-list/universite-list.component';
+import { HomeComponent } from './home/home.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import {NgbCollapseModule, NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UniversiteListComponent,
+    HomeComponent,
+    SideBarComponent,
+    NavbarComponent,
+    AdminLayoutComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbCollapseModule,
+        NgbModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/universite-list/universite-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-universite-list',
+  templateUrl: './universite-list.component.html',
+  styleUrls: ['./universite-list.component.css']
+})
+export class UniversiteListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
